4fd62cd291943b2a15090f123d8506128ff12774
David Mórász <m@mcro.de>
2018-03-12 10:34:39 +0100

    fixed a lot of issues with 2D value slider, now it's working

--- Notui/Behaviors/ValueSlider2D.cs
+++ Notui/Behaviors/ValueSlider2D.cs
@@ -11,11 +11,27 @@ namespace Notui.Behaviors
 {
     public class ValueSlider2D : InteractionBehavior
     {
+        public class BehaviorState : AuxiliaryObject
+        {
+            public override AuxiliaryObject Copy()
+            {
+                return new BehaviorState();
+            }
+
+            public override void UpdateFrom(AuxiliaryObject other) { }
+        }
+
         /// <summary>
         /// Slide speed along the horizontal (X) or the vertical (Y) axis (0 == off)
         /// </summary>
         [BehaviorParameter]
         public Vector2 AxisCoeff { get; set; } = new Vector2(1.0f);
+
+        /// <summary>
+        /// Default values at creation
+        /// </summary>
+        [BehaviorParameter]
+        public Vector2 Default { get; set; } = new Vector2(0.0f);
 
         /// <summary>
         /// Constrain the sliding value to a range
@@ -49,12 +65,29 @@ namespace Notui.Behaviors
 
         public override void Behave(NotuiElement element)
         {
-            if(AxisCoeff.Length() < 0.00001) return;
+            if (element.Value == null)
+            {
+                element.Value = new AttachedValues();
+            }
+
+            var stateAvailable = IsStateAvailable(element);
+            var values = element.Value.Values;
+
+            if (!stateAvailable)
+            {
+                SetState(element, new BehaviorState());
+                element.Value.Values = new float[Max(VerticalOffs, HorizontalOffs) + 1];
+                element.Value.Values.Fill(values);
+                values = element.Value.Values;
+                values[HorizontalOffs] = Default.X;
+                values[VerticalOffs] = Default.Y;
+            }
+
+            if (AxisCoeff.Length() < 0.00001) return;
             if(Constrain && Vector2.Distance(LimitMin, LimitMax) < 0.00001) return;
+            if(element.Touching.IsEmpty) return;
 
             var fastesttouch = element.Touching.Keys.OrderByDescending(t => t.Velocity.LengthSquared()).First();
-            if(element.Value == null) element.Value = new AttachedValues();
-            var values = element.Value.Values;
             if (values.Length <= Max(VerticalOffs, HorizontalOffs))
             {
                 element.Value.Values = new float[Max(VerticalOffs, HorizontalOffs) + 1];
@@ -65,13 +98,13 @@ namespace Notui.Behaviors
             var vel = fastesttouch.GetPlanarVelocity(element.DisplayMatrix, element.Context, out var cpos, out var ppos);
             if(Constrain)
             {
-                values[HorizontalOffs] = Max(LimitMin.X, Min(LimitMax.X, values[HorizontalOffs] + vel.X * AxisCoeff.X));
-                values[VerticalOffs] = Max(LimitMin.Y, Min(LimitMax.Y, values[VerticalOffs] + vel.Y * AxisCoeff.Y));
+                values[HorizontalOffs] = Max(LimitMin.X, Min(LimitMax.X, values[HorizontalOffs] + vel.X * AxisCoeff.X * 0.5f));
+                values[VerticalOffs] = Max(LimitMin.Y, Min(LimitMax.Y, values[VerticalOffs] + vel.Y * AxisCoeff.Y * 0.5f));
             }
             else
             {
-                values[HorizontalOffs] += vel.X * AxisCoeff.X;
-                values[VerticalOffs] += vel.Y * AxisCoeff.Y;
+                values[HorizontalOffs] += vel.X * AxisCoeff.X * 0.5f;
+                values[VerticalOffs] += vel.Y * AxisCoeff.Y * 0.5f;
             }
         }
     }
